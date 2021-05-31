f1eee03155ad1024b69de9741b2a97182e669113
Minas <whiplashoo721@gmail.com>
2021-05-31 19:54:27 +0300

    add flag to register pan events or not

--- lib/src/canvas_touch_detector.dart
+++ lib/src/canvas_touch_detector.dart
@@ -9,8 +9,9 @@ import 'package:touchable/src/types/types.dart';
 /// The [builder] function passes the [BuildContext] and expects a [CustomPaint] object as its return value.
 class CanvasTouchDetector extends StatefulWidget {
   final CustomTouchPaintBuilder builder;
+  final bool shouldRegisterPanEvents;
 
-  const CanvasTouchDetector({Key? key, required this.builder}) : super(key: key);
+  const CanvasTouchDetector({Key? key, required this.builder, required this.shouldRegisterPanEvents}) : super(key: key);
 
   @override
   _CanvasTouchDetectorState createState() => _CanvasTouchDetectorState();
@@ -28,38 +29,61 @@ class _CanvasTouchDetectorState extends State<CanvasTouchDetector> {
   @override
   Widget build(BuildContext context) {
     return TouchDetectionController(touchController, addStreamListener,
-        child: GestureDetector(
-          behavior: HitTestBehavior.translucent,
-          child: Builder(
-            builder: (context) {
-              return widget.builder(context);
-            },
-          ),
-          onTapDown: (tapDetail) {
-            touchController.add(Gesture(GestureType.onTapDown, tapDetail));
-          },
-          onTapUp: (tapDetail) {
-            touchController.add(Gesture(GestureType.onTapUp, tapDetail));
-          },
-          onLongPressStart: (tapDetail) {
-            touchController.add(Gesture(GestureType.onLongPressStart, tapDetail));
-          },
-          onLongPressEnd: (tapDetail) {
-            touchController.add(Gesture(GestureType.onLongPressEnd, tapDetail));
-          },
-          onLongPressMoveUpdate: (tapDetail) {
-            touchController.add(Gesture(GestureType.onLongPressMoveUpdate, tapDetail));
-          },
-          onPanStart: (tapDetail) {
-            //touchController.add(Gesture(GestureType.onPanStart, tapDetail));
-          },
-          onPanUpdate: (tapDetail) {
-            //touchController.add(Gesture(GestureType.onPanUpdate, tapDetail));
-          },
-          onPanDown: (tapDetail) {
-            //touchController.add(Gesture(GestureType.onPanDown, tapDetail));
-          },
-        ));
+        child: widget.shouldRegisterPanEvents
+            ? GestureDetector(
+                behavior: HitTestBehavior.translucent,
+                child: Builder(
+                  builder: (context) {
+                    return widget.builder(context);
+                  },
+                ),
+                onTapDown: (tapDetail) {
+                  touchController.add(Gesture(GestureType.onTapDown, tapDetail));
+                },
+                onTapUp: (tapDetail) {
+                  touchController.add(Gesture(GestureType.onTapUp, tapDetail));
+                },
+                onLongPressStart: (tapDetail) {
+                  touchController.add(Gesture(GestureType.onLongPressStart, tapDetail));
+                },
+                onLongPressEnd: (tapDetail) {
+                  touchController.add(Gesture(GestureType.onLongPressEnd, tapDetail));
+                },
+                onLongPressMoveUpdate: (tapDetail) {
+                  touchController.add(Gesture(GestureType.onLongPressMoveUpdate, tapDetail));
+                },
+                onPanStart: (tapDetail) {
+                  touchController.add(Gesture(GestureType.onPanStart, tapDetail));
+                },
+                onPanUpdate: (tapDetail) {
+                  touchController.add(Gesture(GestureType.onPanUpdate, tapDetail));
+                },
+                onPanDown: (tapDetail) {
+                  touchController.add(Gesture(GestureType.onPanDown, tapDetail));
+                },
+              )
+            : GestureDetector(
+                behavior: HitTestBehavior.translucent,
+                child: Builder(
+                  builder: (context) {
+                    return widget.builder(context);
+                  },
+                ),
+                onTapDown: (tapDetail) {
+                  touchController.add(Gesture(GestureType.onTapDown, tapDetail));
+                },
+                onTapUp: (tapDetail) {
+                  touchController.add(Gesture(GestureType.onTapUp, tapDetail));
+                },
+                onLongPressStart: (tapDetail) {
+                  touchController.add(Gesture(GestureType.onLongPressStart, tapDetail));
+                },
+                onLongPressEnd: (tapDetail) {
+                  touchController.add(Gesture(GestureType.onLongPressEnd, tapDetail));
+                },
+                onLongPressMoveUpdate: (tapDetail) {
+                  touchController.add(Gesture(GestureType.onLongPressMoveUpdate, tapDetail));
+                }));
   }
 
   @override
